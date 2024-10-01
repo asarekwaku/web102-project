83f2dfb75f0b8eea0ba991ec18139261cfd4206f
Kwakuofosujuniorasare <kwakuofosujuniorasare@gmail.com>
2024-10-01 14:31:18 -0400

    Refactor CSS styles and add Card component

--- Japanese-flashcards/src/Card/Card.jsx
+++ Japanese-flashcards/src/Card/Card.jsx
@@ -9,28 +9,39 @@ const Card = ({ card, handleNextCard }) => {
   };
 
   return (
-    <div
-      className={`card-container ${card.level || ''}`}
-      onClick={handleCardClick}
-    >
-      <h2>{isFlipped ? card.question : `How do you say "${card.answer}"?`}</h2>
-      {isFlipped && (
-        <>
-          <p>Romaji: {card.romaji}</p>
-          <p>Level: {card.level}</p>
-        </>
-      )}
+
+    
+    <div className="card-container">
+      <div className="card-scene">
+        <div
+          className={`card ${card.level || ''} ${isFlipped ? 'flipped' : ''}`}
+          onClick={handleCardClick}
+        >
+          {/* Front of the Card */}
+          <div className="card-face card-front">
+            <h2>How do you say "{card.answer}"?</h2>
+            <p>Tap to Flip</p>
+          </div>
+          {/* Back of the Card */}
+          <div className="card-face card-back">
+            <h2>{card.question}</h2>
+            <p>Romaji: {card.romaji}</p>
+            <p>Level: {card.level}</p>
+          </div>
+        </div>
+      </div>
       <button
         className="next-button"
         onClick={(e) => {
           e.stopPropagation(); // Prevent flipping when clicking "Next"
           setIsFlipped(false); // Reset flip state before changing the card
           handleNextCard();
-        }}
-      >
-        Next
+          }}>
+          Next
       </button>
     </div>
+    
+
   );
 };
 
